b6ee7eb4cb7dc5c4435beafee812052166ca4fc4
Alvaro Junior <alvarojunior02.dev@gmail.com>
2026-06-15 00:49:52 -0400

    refactor: move database configuration to dedicated file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { getDatabaseConfig } from './database/database.config';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
         }),
-
         TypeOrmModule.forRootAsync({
             inject: [ConfigService],
-            useFactory: (
-                configService: ConfigService,
-            ): TypeOrmModuleOptions => ({
-                type: 'better-sqlite3',
-                database:
-                    configService.get<string>('DATABASE_NAME') || 'pxbr-breed.sqlite',
-                entities: [__dirname + '/**/*.entity{.ts,.js}'],
-                synchronize:
-                    configService.get<string>('APP_ENV') !== 'production',
-            }),
+            useFactory: getDatabaseConfig,
         }),
     ],
     controllers: [AppController],
